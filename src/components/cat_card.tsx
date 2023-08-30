540e981df3809587a15b896a4e9ff056c73bacad
Camh-git <cameronhillier1@gmail.com>
2023-08-30 22:38:23 +0100

    Activity 5
    The newly variable cat cards and the appropriate call for them

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,9 +1,22 @@
-const CatCard: React.FC = () => (
-  <div className="card">
-    <h3 className="card__text card__header">Mr Gwumpy</h3>
-    <p className="card__text">Species: Cat</p>
-    <p className="card__text">Favourite Food(s): Caviar</p>
-    <p className="card__text">Birth Year: 1984</p>
-  </div>
-);
+interface CatCardProps {
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+}
+
+const CatCard: React.FC<CatCardProps> = (prop) => {
+  /*console.log(
+    "CatCard receiving props from App?! Come in App?! 😸 Props received are: ",
+    prop
+  );*/
+  return (
+    <div className="card">
+      <h3 className="card__text card__header">{prop.name}</h3>
+      <p className="card__text">Species: {prop.species}</p>
+      <p className="card__text">Favourite Food(s): {prop.favFoods}</p>
+      <p className="card__text">Birth Year: {prop.birthYear}</p>
+    </div>
+  );
+};
 export default CatCard;
